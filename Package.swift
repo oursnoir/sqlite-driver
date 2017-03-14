8d910714368087076efbd3fbce63423860c136fd
oursnoir <stephane.calderon@gmail.com>
2017-03-14 11:22:23 +0100

    Point to my fork

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "FluentSQLite",
     dependencies: [
         .Package(url: "https://github.com/vapor/sqlite.git", Version(2,0,0, prereleaseIdentifiers: ["alpha"])),
-        .Package(url: "https://github.com/vapor/fluent.git", Version(2,0,0, prereleaseIdentifiers: ["alpha"])),
+        .Package(url: "https://github.com/oursnoir/fluent.git", Version(2,0,0, prereleaseIdentifiers: ["alpha"])),
     ]
 )
